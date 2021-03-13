5b7a2f12763468f933b5ce61cc4e44d6e656913e
CASL0 <gin.0046.822@gmail.com>
2021-03-13 21:12:45 +0900

    URLパース処理を追加

--- httpClient/HttpClient.cpp
+++ httpClient/HttpClient.cpp
@@ -16,6 +16,10 @@ namespace Win32Util {namespace HttpUtil {
 		void SetHeader(const std::wstring& sHeader);
 		void get(const std::wstring& sUrl);
 
+		//URLをホスト名とパスに分解する
+		//sUrl --> (sHostName, sPath)
+		void ParseUrl(const std::wstring& sUrl, std::wstring& sHostName, std::wstring& sPath);
+
 	public:
 		std::shared_ptr<std::remove_pointer<HINTERNET>::type> m_hSession;
 		std::wstring m_sHeaders;
@@ -39,6 +43,26 @@ namespace Win32Util {namespace HttpUtil {
 
 	void CHttpClient::Impl::get(const std::wstring& sUrl)
 	{
+		std::wstring sHostName;
+		std::wstring sPath;
+		ParseUrl(sUrl, sHostName, sPath);
+	}
+
+	void CHttpClient::Impl::ParseUrl(const std::wstring& sUrl, std::wstring& sHostName, std::wstring& sPath)
+	{
+		URL_COMPONENTS urlComponents = { 0 };
+		urlComponents.dwStructSize = sizeof(URL_COMPONENTS);
+		std::vector<WCHAR> szHost((DWORD)-1);
+		std::vector<WCHAR> szPath((DWORD)-1);
+		urlComponents.lpszHostName = szHost.data();
+		urlComponents.dwHostNameLength = szHost.size();
+		urlComponents.lpszUrlPath = szPath.data();
+		urlComponents.dwUrlPathLength = szPath.size();
+		BOOL bRet = WinHttpCrackUrl(sUrl.c_str(), sUrl.length(), 0, &urlComponents);
+		ThrowLastError(bRet == FALSE, "WinHttpCrackUrl failed");
+
+		sHostName = std::wstring(szHost.data());
+		sPath = std::wstring(szPath.data());
 	}
 
 	CHttpClient::CHttpClient() :pimpl(std::make_shared<Impl>())
